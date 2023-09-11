29157f6c2f1ca06c1fc3e1241b727615942122da
Arc <atandarokib@gmail.com>
2023-09-12 00:14:53 +0100

    updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,8 +7,8 @@ function Hero() {
       <div className="container flex flex-col justify-center items-center gap-5">
         <img src={logo} alt="" />
         <h1 className="font-futuraXBlkBT font-bold text-[#FCF535] text-5xl md:text-5xl text-center ">
-          Uniting Fun, Memes, and{" "}
-          <span className="block mt-3 text-white">Crypto Bullishness!</span>
+          Zegladoid
+          <span className="block mt-3 text-white">3ZX</span>
         </h1>
         <p className="max-w-[500px] text-center text-white text-base font-normal leading-loose tracking-tight">
           The project is fully decentralized and community based. We just do our
