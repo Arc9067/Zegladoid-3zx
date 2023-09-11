cb3c526cc569f00d51954f8b6725f4486148041a
Arc <atandarokib@gmail.com>
2023-09-12 00:20:20 +0100

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <header className="py-4 w-full absolute top-0">
       <div className="container flex justify-between items-center">
         <a href="" className="font-futuraXBlkBT text-3xl font-bold">
-          Zegladiod 3ZX
+          Zegladoid 3ZX
         </a>
         <button className="transition hover:scale-90 cursor-pointer">
           <img src={menu} alt="" />
